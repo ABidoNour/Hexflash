464c46bac8c831247ea99a20dbd40109dc64e558
Abdo-Lah Nour El Deen <abdolahnour2004@gmail.com>
2023-09-25 15:35:09 -0500

    changed nodes

--- Player.gd
+++ Player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 
-@export var speed : float = 700.0
+@export var speed : float = 300
+
 
 const SPRITE_MAP = {
 	Vector2.RIGHT : preload("res://Art/PlayerSprites/PrototypeSide.png"),
